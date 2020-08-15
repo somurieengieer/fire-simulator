8a2c6201152d7bd752f3aac47d0f50cff5630b96
somurie06 <somurie06@gmail.com>
2020-08-15 21:12:33 +0900

    レスポンシブ対応（スマホ）テーブルのタイトルだけスマホで見るとサイズが大きくなるバグ対応

--- src/layout/molecules/PhaseTableItems.tsx
+++ src/layout/molecules/PhaseTableItems.tsx
@@ -25,7 +25,6 @@ export const usePatternTableStyles = makeStyles({
   },
   tableHeadRow: {
     backgroundColor: theme.palette.secondary.main,
-    fontSize: '1em',
   },
   select: {
     height: '2em',
