b73df131aee9affc3202c86bdb0f774b73cfdb98
somurie06 <somurie06@gmail.com>
2020-08-27 09:52:00 +0900

    税金計算のデータを修正

--- src/features/tax/taxInitialData.ts
+++ src/features/tax/taxInitialData.ts
@@ -1,10 +1,10 @@
 import {TaxSet} from "./taxSlice";
 
 const updateBaseForFree = (set: TaxSet) => {
-  set.deductions[6].checked = false //健康保険
-  set.deductions[7].checked = false // 厚生年金
-  set.deductions[8].checked = true // 国民健康保険
-  set.deductions[9].checked = true // 国民年金
+  set.deductions[7].checked = false //健康保険
+  set.deductions[8].checked = false // 厚生年金
+  set.deductions[9].checked = true // 国民健康保険
+  set.deductions[10].checked = true // 国民年金
 }
 const commonBase = (set: TaxSet) => {
   set.personalInfo.age = 33
@@ -14,7 +14,7 @@ const commonBase = (set: TaxSet) => {
 
 const personalUpdates = (): PersonalPattern[] => [
   {
-    // 個人事業主の節税庵
+    // 個人事業主の節税庵（小規模企業共済など）
     num: 1,
     taxSet: (taxSet: TaxSet[]): TaxSet[] => {
       const updateBase = (set: TaxSet) => {
@@ -22,25 +22,25 @@ const personalUpdates = (): PersonalPattern[] => [
         commonBase(set)
         set.incomes[1].amount = 900
         set.incomes[1].deductions[1].amount = 200
-        set.deductions[1].amount = 0
-        set.deductions[2].amount = 27.6
+        set.deductions[2].amount = 0
+        set.deductions[3].amount = 27.6
       }
       [0, 1, 2].forEach(i => updateBase(taxSet[i]))
 
-      taxSet[1].deductions[1].amount = 84
+      taxSet[1].deductions[2].amount = 84 // 小規模共済に加入した場合
 
       return taxSet
     }
   },
   {
-    // 法人時のパターン
+    // 法人・賃貸マンション暮らし
     num: 2,
     taxSet: (taxSet: TaxSet[]): TaxSet[] => {
       const updateBase = (set: TaxSet) => {
         commonBase(set)
         set.incomes[0].amount = 440 // 900万売上、200万家賃、100万旅費、100万経費、社会保険料等60万？
-        set.deductions[1].amount = 0
-        set.deductions[2].amount = 27.6
+        set.deductions[2].amount = 0
+        set.deductions[3].amount = 27.6
       }
       [0, 1, 2].forEach(i => updateBase(taxSet[i]))
 
@@ -60,15 +60,15 @@ const personalUpdates = (): PersonalPattern[] => [
       const updateBase = (set: TaxSet) => {
         commonBase(set)
         set.incomes[0].amount = 650 // 900万売上、100万旅費、100万経費、社会保険料等60万？
-        set.deductions[1].amount = 0
-        set.deductions[2].amount = 27.6
+        set.deductions[2].amount = 0
+        set.deductions[3].amount = 27.6
       }
       [0, 1, 2].forEach(i => updateBase(taxSet[i]))
 
-      taxSet[1].incomes[0].amount = 300
+      taxSet[1].incomes[0].amount = 300 // 夫婦で300万ずつ給与のパターン
 
-      taxSet[2].incomes[0].amount = 550
-      taxSet[2].incomes[2].amount = 150 // 2000万貸付、300万利子。5%運用として100万利益のため給与から200万減らす
+      taxSet[2].incomes[0].amount = 550 // 一人で受け取って雑所得
+      taxSet[2].incomes[2].amount = 150 // 1000万貸付、150万利子。5%運用として50万利益のため給与から100万減らす
 
       return taxSet
     }
@@ -80,19 +80,19 @@ const personalUpdates = (): PersonalPattern[] => [
       const updateBase = (set: TaxSet) => {
         commonBase(set)
         set.incomes[0].amount = 360 // 900万売上、100万旅費、100万経費、社会保険料等60万？
-        set.deductions[1].amount = 0
-        set.deductions[2].amount = 27.6
+        set.deductions[2].amount = 0
+        set.deductions[3].amount = 27.6
       }
       [0, 1, 2].forEach(i => updateBase(taxSet[i]))
 
       taxSet[0].incomes[0].amount = 720
-      taxSet[0].deductions[5].checked = true // 配偶者控除
+      taxSet[0].deductions[6].checked = true // 配偶者控除
 
       return taxSet
     }
   },
   {
-    // 妻パターン
+    // 妻パターン（法人で役員報酬120万。社会保険料加入）
     num: 10,
     taxSet: (taxSet: TaxSet[]): TaxSet[] => {
       const updateBase = (set: TaxSet) => {
@@ -100,16 +100,16 @@ const personalUpdates = (): PersonalPattern[] => [
         set.personalInfo.age = 37
         set.personalInfo.numberOfFamily = 0
         set.incomes[0].amount = 500
-        set.deductions[1].amount = 84
-        set.deductions[2].amount = 27.6
+        set.deductions[2].amount = 84
+        set.deductions[3].amount = 27.6
         set.retirementTax.workingYears = 20
       }
       [0, 1, 2].forEach(i => updateBase(taxSet[i]))
-      taxSet[1].deductions[1].amount = 0
+      taxSet[1].deductions[2].amount = 0
 
-      taxSet[2].deductions[6].checked = false
-      taxSet[2].deductions[6].amount = 13
       taxSet[2].deductions[7].checked = false
+      taxSet[2].deductions[7].amount = 13
+      taxSet[2].deductions[8].checked = false
 
       return taxSet
     }
