4a05a03c2da8b015c7c2960cbe8a563988e64587
somurie06 <somurie06@gmail.com>
2020-09-18 13:05:26 +0900

    トップにスクロールするリンクを追加

--- src/layout/atoms/NonDecoratedLink.tsx
+++ src/layout/atoms/NonDecoratedLink.tsx
@@ -17,11 +17,20 @@ interface Props {
   children: React.ReactNode;
 }
 
-// 複利計算ページ
 export function NonDecoratedLink ({ url, children }: Props) {
   const classes = useStyles()
+
+  const topScroll = () => {
+    if (!document.scrollingElement) return
+    if (document.scrollingElement.scrollTop < 10) {
+      document.scrollingElement.scrollTop = 0
+    } else {
+      document.scrollingElement.scrollTop = document.scrollingElement.scrollTop / 1.1
+      setTimeout(topScroll, 10)
+    }
+  }
   return (
-    <Link to={url} className={classes.root}>
+    <Link to={url} className={classes.root} onClick={topScroll}>
       {children}
     </Link>
   )
