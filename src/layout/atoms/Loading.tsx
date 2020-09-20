c1b7558c5b35450307cb77793c5cd168702066e0
somurie06 <somurie06@gmail.com>
2020-09-20 17:51:13 +0900

    Loading画像が表示されるようテスト

--- src/layout/atoms/Loading.tsx
+++ src/layout/atoms/Loading.tsx
@@ -7,13 +7,13 @@ import { AlignCenterBox } from './AlignCenterBox'
 
 export function Loading () {
   return (
-    <div>LoadingDiv</div>
+    <JustifyCenterBox>
+      <div>LoadingDiv</div>
+    </JustifyCenterBox>
     // ローディングアイコン
-    // <JustifyCenterBox>
     //   <AlignCenterBox height={400}>
     //     {/* <FontAwesomeIcon className='fa-spin' style={{ padding: 10, fontSize: 50 }} icon={faSpinner} /> */}
   // <Typography>Loading</Typography>
   // </AlignCenterBox>
-    // </JustifyCenterBox>
   )
 }
